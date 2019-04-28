e2b32607131803ccd3b3e15e92dc88bba982d223
Mike Goldin <mike.goldin@protonmail.ch>
2019-04-28 13:08:10 -0400

    Implement disbursals in the scanloop

--- server/scanloop.js
+++ server/scanloop.js
@@ -1,9 +1,27 @@
 const axios = require('axios');
 
 /* eslint-disable no-param-reassign */
+function prepareNextRelease(node) {
+  node.nextReleaseDate = (() => {
+    const interval = Math.floor(Math.random() * node.remainingTtl);
+    node.remainingTtl -= interval;
+    return node.releaseBy - interval;
+  })();
+  node.nextReleaseAmt = (() => {
+    let releaseAmt;
+    if (node.remainingAddrs.length === 1) {
+      releaseAmt = node.remainingDeposit;
+    } else {
+      releaseAmt = Math.random() * node.remainingDeposit;
+    }
+    node.remainingDeposit -= releaseAmt;
+    return releaseAmt;
+  })();
+}
+
+/* eslint-disable no-param-reassign */
 function scanloop(mixes, houseAddr) {
   mixes.forEach(async (node) => {
-
     // The first thing we'll check is if a mix still hasn't received its
     // deposit, whether a deposit has become available to transfer in to the
     // house address.
@@ -23,9 +41,13 @@ function scanloop(mixes, houseAddr) {
         });
 
         // Update the mix node
+        const now = Date.now();
         node.depositAmt = depositAmt;
-        node.releaseBy = Date.now() + node.ttl;
+        node.releaseBy = now + node.ttl;
+        node.remainingDeposit = node.depositAmt;
+        node.remainingTtl = node.ttl;
         node.remainingAddrs = node.userAddrs;
+        prepareNextRelease(node);
       }
     }
 
@@ -34,7 +56,18 @@ function scanloop(mixes, houseAddr) {
 
     // Next, we'll check if a mix has a release ready. If so, we'll make the
     // release and compute parameters for the next one.
-    
+    if (node.nextReleaseDate < Date.now() && node.remainingAddrs.length > 0) {
+      await axios.post('http://jobcoin.gemini.com/crowbar/api/transactions', {
+        fromAddress: houseAddr,
+        toAddress: node.remainingAddrs.pop(),
+        amount: node.nextReleaseAmt,
+      });
+
+      if (node.remainingAddrs.length > 0) {
+        prepareNextRelease(node);
+      }
+    }
+
     // If the length of remainingAddrs is zero, this mix is complete and we can
     // delete it.
   });
